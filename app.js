219b6fee2d0da7122cbaa491642ce4d0dbab916c
PrincessAcosta <103625104+PrincessAcosta@users.noreply.github.com>
2022-10-22 19:38:23 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const read = require('./read')
+const add = require('./add')
+
+const cmd = process.argv
+
+if(cmd[2] == 'add') {
+
+    const note = {
+        id: cmd[3],
+        title: cmd[4],
+        body: cmd[5],
+    }
+    const oldNote = read()
+
+    add(note, oldNote)
+}
+if(cmd[2] == 'read' ) {
+    console.log(read())
+}
+if(cmd[2]== 'delete'){
+    console.log("deletefunction")
+    const id = cmd[3]
+    const oldNote = read()
+    const del = require('./del')
+    del(id, oldNote);
+    console.log(read());
+}
